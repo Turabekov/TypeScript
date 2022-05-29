ebc63a6da1ff7f56f318e72a499ab24761bcc390
Xumoyun Turabekov <turabekov1110@gmail.com>
2022-05-29 20:48:48 +0500

    Types in functions

--- 1. Main types/2TypesInFunction/app.ts
+++ 1. Main types/2TypesInFunction/app.ts
@@ -1,1 +1,9 @@
+function getFullName(firstname: string, surname: string): string {
+    return `${firstname} ${surname}`;
+}
 
+const getFullNameArrow = (firstname: string, surname: string): string => {
+    return `${firstname} ${surname}`;
+};
+
+console.log(getFullName("Xumoyun", "Turabekov"));
---
+++ README.md
@@ -0,0 +1,5 @@
+# TypeScript
+
+## TypeScript is JavaScript with syntax for types.
+
+TypeScript is a strongly typed programming language that builds on JavaScript, giving you better tooling at any scale.
