cb845698bfa5741821787c51770d2c6c381aeef0
Xumoyun Turabekov <turabekov1110@gmail.com>
2022-05-30 20:29:18 +0500

    Tuples

---
+++ 1. Main types/5Tuples/app.ts
@@ -0,0 +1,12 @@
+const skill: [number, string] = [1, "Dev"];
+// const id = skill[0];
+// const skillName = skill[1];
+// // ! error const q = skill[2]
+
+// skill.push("dfsfdf");
+// // ! error const dsf = skill[2];
+// skill.pop();
+
+const [id, skillName] = skill;
+
+const arr: [number, string, ...boolean[]] = [1, "sad", true, true, false];
