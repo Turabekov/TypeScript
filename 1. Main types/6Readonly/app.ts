2517c5edece99f83262092cff6d168b600b52a66
Xumoyun Turabekov <turabekov1110@gmail.com>
2022-05-31 18:09:01 +0500

    Readonly

---
+++ 1. Main types/6Readonly/app.ts
@@ -0,0 +1,9 @@
+const skill2: readonly [number, string] = [1, "Dev"];
+//  with
+// skill2[0] = "wd"; we cannot require
+
+const skills3: ReadonlyArray<string> = ["Dev", "DevOps"];
+
+// skills3[0] = "";
+
+// skills3.push();
