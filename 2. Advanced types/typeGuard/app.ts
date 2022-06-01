0a662ccb5563b91a94fb04aeca5e0ee4c215653e
Xumoyun Turabekov <turabekov1110@gmail.com>
2022-06-01 21:51:11 +0500

    type Guard

---
+++ 2. Advanced types/typeGuard/app.ts
@@ -0,0 +1,44 @@
+interface UserB {
+    name: string;
+    email: string;
+    login: string;
+}
+
+const userB: UserB = {
+    name: "Vasya",
+    email: "vassiliy@yandex.ru",
+    login: "vasia",
+};
+
+interface AdminB {
+    name: string;
+    role: number;
+}
+
+function logId(id: string | number) {
+    if (isString(id)) {
+        console.log(id);
+    } else {
+        console.log(id);
+    }
+}
+
+function isString(x: string | number): x is string {
+    return typeof id === "string";
+}
+
+function isAdmin(user: UserB | AdminB): user is AdminB {
+    return "role" in user;
+}
+
+function isAdminAlternative(user: UserB | AdminB): user is AdminB {
+    return (user as AdminB).role !== undefined;
+}
+
+function setRoleZero(user: UserB | AdminB) {
+    if (isAdmin(user)) {
+        user.role = 0;
+    } else {
+        throw new Error("User not Admin");
+    }
+}
