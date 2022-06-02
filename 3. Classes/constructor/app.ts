5991ffb2792557cbe0892dd567037cbb3f75e0e5
Xumoyun Turabekov <turabekov1110@gmail.com>
2022-06-02 17:48:21 +0500

    constructor

--- 3. Classes/constructor/app.ts
+++ 3. Classes/constructor/app.ts
@@ -1,9 +1,24 @@
 class User12 {
     name: string;
+    age: number;
 
-    constructor(name: string) {
-        this.name = name;
+    constructor();
+    constructor(name: string);
+    constructor(age: number);
+    constructor(name: string, age: number);
+
+    constructor(ageOrName?: string | number, age?: number) {
+        if (typeof ageOrName === "string") {
+            this.name = ageOrName;
+        } else if (typeof ageOrName === "number") {
+            this.age = ageOrName;
+        }
+        if (typeof age === "number") {
+            this.age = age;
+        }
     }
 }
 
-const user12 = new User11("Вася");
+const user12 = new User12("Вася");
+const user13 = new User12();
+const user14 = new User12(33);
