cf0229edb7359bdc4d6ed9ebda6a93b184a63df8
Xumoyun Turabekov <turabekov1110@gmail.com>
2022-06-02 18:47:44 +0500

    exercise 4 added

--- practise/exercise4/app.ts
+++ practise/exercise4/app.ts
@@ -1,6 +1,27 @@
-// type Payment = number | { sum: number };
+class User15 {
+    skills: string[];
 
-// let payment: Payment = 10;
-// payment = { sum: 10 };
+    addSkill(skill: string): void;
+    addSkill(skills: string[]): void;
+    addSkill(skillOrSkills: string | string[]): void {
+        if (typeof skillOrSkills == "string") {
+            this.skills.push(skillOrSkills);
+        } else {
+            this.skills.concat(skillOrSkills);
+        }
+    }
+}
 
-const userName = "Вася";
+new User15().addSkill("ddsds");
+
+function run(distance: string): string;
+function run(distance: number): number;
+function run(distance: number | string): number | string {
+    if (typeof distance == "number") {
+        return 1;
+    } else {
+        return "";
+    }
+}
+
+run(12);
